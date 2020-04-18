cb5225d9f2ae4c3f90334b4b66e40b9a1af4189d
Sonicboomcolt <littiled3131@gmail.com>
2020-04-18 02:52:02 -0400

    Updated player
    Updated player stuff and cam

--- Ludum Dare 46/Assets/Scripts/Player_Controllers.cs
+++ Ludum Dare 46/Assets/Scripts/Player_Controllers.cs
@@ -10,23 +10,27 @@ public class Player_Controllers : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float turnSpeed = 5f;
+    [SerializeField] private float gavitySpeed = 20f;
 
     private float InputX;
     private float InputY;
 
     private void Awake()
     {
+        //Grab the controller
         controller = GetComponent<CharacterController>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        //Rotate the player based on input
         transform.Rotate(0.0f, Input.GetAxis("Horizontal") * turnSpeed, 0.0f);
 
         //InputX = Input.GetAxisRaw("Horizontal");
-        InputY = Input.GetAxisRaw("Vertical");
+        InputY = Input.GetAxis("Vertical");
 
-        MoveDir = new Vector3(InputX, 0, InputY);
+        //Move the player
+        MoveDir = new Vector3(InputX, -gavitySpeed, InputY);
         MoveDir = transform.TransformDirection(MoveDir);
 
         controller.Move(MoveDir * moveSpeed * Time.deltaTime);
