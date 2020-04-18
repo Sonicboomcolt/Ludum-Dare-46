25007a225e1287879eed7d052dcdaef7865a85f6
MrKruckerYT <63903071+MrKruckerYT@users.noreply.github.com>
2020-04-18 07:26:31 -0500

    UIDONE
    ok

---
+++ Ludum Dare 46/Assets/Levels/Kruckers Scripts/UISCRIPT.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISCRIPT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
